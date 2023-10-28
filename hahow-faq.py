bbc510ec902e2cabc7794524b2a3331919d12a0b
Alice Chen <chiachun78@gmail.com>
2023-10-28 16:57:33 +0800

    Calculate relatedness and search articles

--- hahow-faq.py
+++ hahow-faq.py
@@ -2,15 +2,18 @@ import os
 import json
 import requests
 import string
+import ast
 import tiktoken
 import openai
 import pandas as pd
+from scipy import spatial
 
 MISO_APP_TOKEN = os.environ['MISO_OTHERS_API_KEY']
 USER_ID = os.environ['USER_ID']
 MISO_BASE_URL = 'https://api-edge.askmiso.com'
 GPT_MODEL = "gpt-3.5-turbo"
 EMBEDDING_MODEL = "text-embedding-ada-002"
+EMBEDDING_PATH = "data/faq_embeddings.csv"
 
 def num_tokens(text: str, model: str = GPT_MODEL) -> int:
   encoding = tiktoken.encoding_for_model(model)
@@ -43,13 +46,72 @@ def calculate_embeddings(documents: list):
   response = openai.Embedding.create(model=EMBEDDING_MODEL, input=documents)
   embeddings = [e["embedding"] for e in response["data"]]
   df = pd.DataFrame({"text": documents, "embedding": embeddings})
-  df.to_csv("data/faq_embeddings.csv", index=False)
+  df.to_csv(EMBEDDING_PATH, index=False)
 
-documents = fetch_articles()
-calculate_embeddings(documents)
+def qa_without_rag():
+  """
+  q1 = 'Hahowise 學習助教是誰？'
+  q2 = 'Hahowise 要怎麼使用？'
+  q3 = 'Hahow 2023 有什麼新功能？'
+  """
+  q3 = 'Hahow 2023 有什麼新功能？'
+  """
+  很抱歉，我無法提供有關 Hahow 2023 的具體資訊，因為我是一個 AI 助手，無法預測未來的功能更新。建議您直接向 Hahow 官方網站或客服詢問，以獲得最準確的資訊。
+  """
+  response = openai.ChatCompletion.create(
+    messages=[
+        {'role': 'system', 'content': 'You answer questions about Hahow'},
+        {'role': 'user', 'content': q3},
+    ],
+    model=GPT_MODEL,
+    temperature=0,
+  )
+  print(response['choices'][0]['message']['content'])
 
-# token check
+def def_setup():
+  df = pd.read_csv(EMBEDDING_PATH)
+  df['embedding'] = df['embedding'].apply(ast.literal_eval)
+  return df
+
+def searc_related_articles(
+  query: str,
+  df: pd.DataFrame,
+  relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
+  top_n: int = 10
+) -> tuple[list[str], list[float]]:
+  query_embedding_response = openai.Embedding.create(
+      model=EMBEDDING_MODEL,
+      input=query,
+  )
+  query_embedding = query_embedding_response["data"][0]["embedding"]
+  strings_and_relatednesses = [
+      (row["text"], relatedness_fn(query_embedding, row["embedding"]))
+      for i, row in df.iterrows()
+  ]
+  strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+  strings, relatednesses = zip(*strings_and_relatednesses)
+  return strings[:top_n], relatednesses[:top_n]
+
+"""step 1: fetch data"""
+# documents = fetch_articles()
+
+"""step 2: token check"""
 # token_check(documents)
 
+"""step 3: calculate embeddings"""
+# calculate_embeddings(documents)
 
+"""step 4: test without RAG"""
+# qa_without_rag()
+
+"""step 5: set up RAG"""
+df = def_setup()
+
+"""step 6: calculate relatedness"""
+strings, relatednesses = searc_related_articles("要如何取得完課證明？", df, top_n=5)
+for string, relatedness in zip(strings, relatednesses):
+  print(f"{relatedness=:.3f}")
+  print(string)
+
+"""step 7: ask"""
 
